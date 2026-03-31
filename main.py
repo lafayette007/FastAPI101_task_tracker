2f1850e6eff84f37193cd4dc1cae7d9d66452813
lafayette007 <anton.m.kalinin@gmail.com>
2026-03-31 19:53:04 +0300

    Fast API 101 pydantic schema for add_task

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import (
+    FastAPI,
+    Depends             # инъекция зависимостей
+)
+from pydantic import BaseModel
+from sqlalchemy.sql.annotation import Annotated
 
 app = FastAPI()
 
-@app.get ("/home")
-def get_homepage():
-    return {"data": "Hello world!"}
+# это схемы в Pydantic - для проверки типов данных и ввода значений полей
+class STaskAdd(BaseModel):
+    name: str
+    description: str | None
+
+class STask(STaskAdd):
+    id: int
+
+tasks = []
+
+@app.post("/tasks")
+async def add_task(
+        task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {"Ok": True}
+
+# @app.get("/tasks")
+# def get_tasks():
+#     task = STask(name="Создай Strategy OS", description="Система стратегического планирования")
+#     return {"data": task}
