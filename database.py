c7791cb9611df1ce54c38cbbc06d5c007bb6e45d
lafayette007 <anton.m.kalinin@gmail.com>
2026-03-31 21:25:47 +0300

    Fast API 101 19:44 https://www.youtube.com/watch?v=gBfkX9H3szQ
    database.py
    - Base - декларативное создание таблиц БД
    - таблица Tasks
    - create_tables()
    - delete tables()
    
    main.py
    lifespan - удаляет и создаёт все таблицы БД при каждом перезапуске приложения

---
+++ database.py
@@ -0,0 +1,42 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,                # движок для БД
+    async_sessionmaker                  # фабрика создания сессий (транзакций для работы с БД)
+)
+from sqlalchemy.orm import (
+    DeclarativeBase,                    # декларативное создание таблиц
+    Mapped,                             # для объявления типов полей
+    mapped_column                       # для полей типа Column
+)
+
+# движок для подключения к БД
+engine = create_async_engine(
+    url="sqlite+aiosqlite:///tasks.db"
+)
+
+# фабрика сессий для работы с БД
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+# таблицы БД
+# базовый класс всех остальных таблиц - от него будем их наследовать
+# 1 таблица БД = 1 класс
+class Base(DeclarativeBase):
+    pass
+
+# таблица Задачи
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)   # указываем что это поле - первичный ключ таблицы (нам обязательно нужен хотя бы один)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str | None] = mapped_column()
+
+# создание таблиц - эта АСИНХРОННАЯ функция берётся из доки по SQLAlchemy
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# удаление таблиц - обычно не используется (т.к. таблицы БД это очень ценная инфа)
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
--- main.py
+++ main.py
@@ -3,28 +3,50 @@ from fastapi import (
     Depends             # инъекция зависимостей
 )
 from pydantic import BaseModel
-from sqlalchemy.sql.annotation import Annotated
+from typing import Annotated
 
-app = FastAPI()
+# https://fastapi.tiangolo.com/advanced/events/ - копируем код отсюда про Lifespan (жизненный цикл)
+from contextlib import asynccontextmanager  # декоратор, который позволяет создавать контекстные менеджеры со словом yield
 
+from database import create_tables, delete_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # вставим 2 ф-ции -
+    # 1) удалять все старые таблицы при старте приложения - просто сейчас так удобно
+    # 2) создавать все новые таблицы при старте приложения
+    await delete_tables()
+    print("База таблиц очищена")
+    await create_tables()
+    print("База таблиц готова к работе")
+
+    yield
+    print("Выключение приложения")
+
+app = FastAPI(lifespan=lifespan)
+ 
 # это схемы в Pydantic - для проверки типов данных и ввода значений полей
-class STaskAdd(BaseModel):
+class TaskAddSchema(BaseModel):
     name: str
     description: str | None
 
-class STask(STaskAdd):
+
+class TaskSchema(TaskAddSchema):
     id: int
+
 
 tasks = []
 
 @app.post("/tasks")
 async def add_task(
-        task: Annotated[STaskAdd, Depends()],
+        # task: TaskAddSchema
+        task: Annotated[TaskAddSchema, Depends()],
 ):
     tasks.append(task)
     return {"Ok": True}
 
 # @app.get("/tasks")
 # def get_tasks():
-#     task = STask(name="Создай Strategy OS", description="Система стратегического планирования")
+#     task = TaskSchema(name="Создай Strategy OS", description="Система стратегического планирования")
 #     return {"data": task}
